4677f5ec6a46007e854a9a4d33e32b1820af09bc
malcano <steveleemh@gmail.com>
2022-10-01 04:25:17 +0000

    add mysql database and modify related py code

--- Back/config/settings.py
+++ Back/config/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', #FIX
+        'NAME': 'SBH_DATABASE', #Name of DB
+        'USER': 'forall',
+        'PASSWORD': '1234'
     }
 }
 
